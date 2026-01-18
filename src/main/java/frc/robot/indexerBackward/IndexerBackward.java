d9d5d4a11d6b56d79caa97c3163045a30bdd0772
Spectrum3847 <spectrum3847@gmail.com>
2026-01-17 20:28:21 -0600

    temp bring up indexers

---
+++ src/main/java/frc/robot/indexerBackward/IndexerBackward.java
@@ -0,0 +1,154 @@
+package frc.robot.indexerBackward;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NTSendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotSim;
+import frc.spectrumLib.Rio;
+import frc.spectrumLib.Telemetry;
+import frc.spectrumLib.mechanism.Mechanism;
+import frc.spectrumLib.sim.RollerConfig;
+import frc.spectrumLib.sim.RollerSim;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class IndexerBackward extends Mechanism {
+
+    public static class IndexerBackwardConfig extends Config {
+
+        // Intake Voltages and Current
+        @Getter @Setter private double indexerBackwardVoltage = 9.0;
+        @Getter @Setter private double indexerBackwardCurrent = 30.0;
+        @Getter @Setter private double indexerBackwardTorqueCurrent = 85.0;
+
+        /* Intake config values */
+        @Getter private double currentLimit = 44;
+        @Getter private double torqueCurrentLimit = 200;
+        @Getter private double velocityKp = 12;
+        @Getter private double velocityKv = 0.2;
+        @Getter private double velocityKs = 14;
+
+        /* Sim Configs */
+        @Getter private double intakeX = Units.inchesToMeters(10);
+        @Getter private double intakeY = Units.inchesToMeters(12);
+        @Getter private double wheelDiameter = 4;
+
+        public IndexerBackwardConfig() {
+            super("IndexerBackward", 47, Rio.CANIVORE);
+            configPIDGains(0, velocityKp, 0, 0);
+            configFeedForwardGains(velocityKs, velocityKv, 0, 0);
+            configGearRatio(1);
+            configSupplyCurrentLimit(currentLimit, true);
+            configStatorCurrentLimit(torqueCurrentLimit, true);
+            configForwardTorqueCurrentLimit(torqueCurrentLimit);
+            configReverseTorqueCurrentLimit(torqueCurrentLimit);
+            configNeutralBrakeMode(true);
+            configCounterClockwise_Positive();
+        }
+    }
+
+    private IndexerBackwardConfig config;
+    private indexerBackwardSim sim;
+
+    public IndexerBackward(IndexerBackwardConfig config) {
+        super(config);
+        this.config = config;
+
+        simulationInit();
+        telemetryInit();
+        Telemetry.print(getName() + " Subsystem Initialized");
+    }
+
+    @Override
+    public void periodic() {}
+
+    @Override
+    public void setupStates() {}
+
+    @Override
+    public void setupDefaultCommand() {
+        IndexerBackwardStates.setupDefaultCommand();
+    }
+
+    /*-------------------
+    initSendable
+    Use # to denote items that are settable
+    ------------*/
+
+    @Override
+    public void initSendable(NTSendableBuilder builder) {
+        if (isAttached()) {
+            builder.addStringProperty("CurrentCommand", this::getCurrentCommandName, null);
+            builder.addDoubleProperty("Motor Voltage", this::getVoltage, null);
+            builder.addDoubleProperty("Rotations", this::getPositionRotations, null);
+            builder.addDoubleProperty("Velocity RPM", this::getVelocityRPM, null);
+            builder.addDoubleProperty("StatorCurrent", this::getStatorCurrent, null);
+        }
+    }
+
+    // --------------------------------------------------------------------------------
+    // Custom Commands
+    // --------------------------------------------------------------------------------
+    
+    public Command runTorqueFOC(DoubleSupplier torque) {
+        return run(() -> setTorqueCurrentFoc(torque));
+    }
+
+    public void setVoltageAndCurrentLimits(
+            DoubleSupplier voltage, DoubleSupplier supply, DoubleSupplier torque) {
+        setVoltageOutput(voltage);
+        setCurrentLimits(supply, torque);
+    }
+
+    public Command runVoltageCurrentLimits(
+            DoubleSupplier voltage, DoubleSupplier supplyCurrent, DoubleSupplier torqueCurrent) {
+        return runVoltage(voltage).alongWith(runCurrentLimits(supplyCurrent, torqueCurrent));
+    }
+
+    public Command runTCcurrentLimits(DoubleSupplier torqueCurrent, DoubleSupplier supplyCurrent) {
+        return runTorqueCurrentFoc(torqueCurrent)
+                .alongWith(runCurrentLimits(supplyCurrent, torqueCurrent));
+    }
+
+    public Command stopMotor() {
+        return run(() -> stop());
+    }
+
+    // --------------------------------------------------------------------------------
+    // Simulation
+    // --------------------------------------------------------------------------------
+    public void simulationInit() {
+        if (isAttached()) {
+            // Create a new RollerSim with the left view, the motor's sim state, and a 6 in diameter
+            sim = new indexerBackwardSim(RobotSim.leftView, motor.getSimState());
+        }
+    }
+
+    // Must be called to enable the simulation
+    // if roller position changes configure x and y to set position.
+    @Override
+    public void simulationPeriodic() {
+        if (isAttached()) {
+            sim.simulationPeriodic();
+        }
+    }
+
+    class indexerBackwardSim extends RollerSim {
+        public indexerBackwardSim(Mechanism2d mech, TalonFXSimState rollerMotorSim) {
+            super(
+                    new RollerConfig(config.getWheelDiameter())
+                            .setPosition(config.getIntakeX(), config.getIntakeY())
+                            .setMount(Robot.getIntakeExtension().getSim()),
+                    mech,
+                    rollerMotorSim,
+                    config.getName());
+        }
+    }
+}
---
+++ src/main/java/frc/robot/indexerForward/IndexerForward.java
@@ -0,0 +1,154 @@
+package frc.robot.fuelIntake;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NTSendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotSim;
+import frc.spectrumLib.Rio;
+import frc.spectrumLib.Telemetry;
+import frc.spectrumLib.mechanism.Mechanism;
+import frc.spectrumLib.sim.RollerConfig;
+import frc.spectrumLib.sim.RollerSim;
+
+import java.util.function.DoubleSupplier;
+
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class IndexerForward extends Mechanism {
+
+    public static class FuelIntakeConfig extends Config {
+
+        // Intake Voltages and Current
+        @Getter @Setter private double fuelIntakeVoltage = 9.0;
+        @Getter @Setter private double fuelIntakeSupplyCurrent = 30.0;
+        @Getter @Setter private double fuelIntakeTorqueCurrent = 85.0;
+
+        /* Intake config values */
+        @Getter private double currentLimit = 44;
+        @Getter private double torqueCurrentLimit = 200;
+        @Getter private double velocityKp = 12;
+        @Getter private double velocityKv = 0.2;
+        @Getter private double velocityKs = 14;
+
+        /* Sim Configs */
+        @Getter private double intakeX = Units.inchesToMeters((RobotSim.getLeftViewWidth() / 2) - 15);
+        @Getter private double intakeY = Units.inchesToMeters((RobotSim.getLeftViewWidth() / 2) - 5);
+        @Getter private double wheelDiameter = 6;
+
+        public FuelIntakeConfig() {
+            super("Intake", 5, Rio.CANIVORE);
+            configPIDGains(0, velocityKp, 0, 0);
+            configFeedForwardGains(velocityKs, velocityKv, 0, 0);
+            configGearRatio(1);
+            configSupplyCurrentLimit(currentLimit, true);
+            configStatorCurrentLimit(torqueCurrentLimit, true);
+            configForwardTorqueCurrentLimit(torqueCurrentLimit);
+            configReverseTorqueCurrentLimit(torqueCurrentLimit);
+            configNeutralBrakeMode(true);
+            configCounterClockwise_Positive();
+        }
+    }
+
+    private FuelIntakeConfig config;
+    private FuelIntakeSim sim;
+
+    public IndexerForward(FuelIntakeConfig config) {
+        super(config);
+        this.config = config;
+
+        simulationInit();
+        telemetryInit();
+        Telemetry.print(getName() + " Subsystem Initialized");
+    }
+
+    @Override
+    public void periodic() {}
+
+    @Override
+    public void setupStates() {}
+
+    @Override
+    public void setupDefaultCommand() {
+        IndexerBackwardStates.setupDefaultCommand();
+    }
+
+    /*-------------------
+    initSendable
+    Use # to denote items that are settable
+    ------------*/
+
+    @Override
+    public void initSendable(NTSendableBuilder builder) {
+        if (isAttached()) {
+            builder.addStringProperty("CurrentCommand", this::getCurrentCommandName, null);
+            builder.addDoubleProperty("Motor Voltage", this::getVoltage, null);
+            builder.addDoubleProperty("Rotations", this::getPositionRotations, null);
+            builder.addDoubleProperty("Velocity RPM", this::getVelocityRPM, null);
+            builder.addDoubleProperty("StatorCurrent", this::getStatorCurrent, null);
+        }
+    }
+
+    // --------------------------------------------------------------------------------
+    // Custom Commands
+    // --------------------------------------------------------------------------------
+    
+    public Command runTorqueFOC(DoubleSupplier torque) {
+        return run(() -> setTorqueCurrentFoc(torque));
+    }
+
+    public void setVoltageAndCurrentLimits(
+            DoubleSupplier voltage, DoubleSupplier supply, DoubleSupplier torque) {
+        setVoltageOutput(voltage);
+        setCurrentLimits(supply, torque);
+    }
+
+    public Command runVoltageCurrentLimits(
+            DoubleSupplier voltage, DoubleSupplier supplyCurrent, DoubleSupplier torqueCurrent) {
+        return runVoltage(voltage).alongWith(runCurrentLimits(supplyCurrent, torqueCurrent));
+    }
+
+    public Command runTCcurrentLimits(DoubleSupplier torqueCurrent, DoubleSupplier supplyCurrent) {
+        return runTorqueCurrentFoc(torqueCurrent)
+                .alongWith(runCurrentLimits(supplyCurrent, torqueCurrent));
+    }
+
+    public Command stopMotor() {
+        return run(() -> stop());
+    }
+
+    // --------------------------------------------------------------------------------
+    // Simulation
+    // --------------------------------------------------------------------------------
+    public void simulationInit() {
+        if (isAttached()) {
+            // Create a new RollerSim with the left view, the motor's sim state, and a 6 in diameter
+            sim = new FuelIntakeSim(RobotSim.leftView, motor.getSimState());
+        }
+    }
+
+    // Must be called to enable the simulation
+    // if roller position changes configure x and y to set position.
+    @Override
+    public void simulationPeriodic() {
+        if (isAttached()) {
+            sim.simulationPeriodic();
+        }
+    }
+
+    class FuelIntakeSim extends RollerSim {
+        public FuelIntakeSim(Mechanism2d mech, TalonFXSimState rollerMotorSim) {
+            super(
+                    new RollerConfig(config.getWheelDiameter())
+                            .setPosition(config.getIntakeX(), config.getIntakeY())
+                            .setMount(Robot.getIntakeExtension().getSim()),
+                    mech,
+                    rollerMotorSim,
+                    config.getName());
+        }
+    }
+}
