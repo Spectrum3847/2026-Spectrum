f915202199e76a087b838519ddc17e8c73c3ea64
Spectrum3847 <spectrum3847@gmail.com>
2026-01-27 18:39:43 -0600

    look ahead pose for shot ballistics

--- src/main/java/frc/rebuilt/ShotCalculator.java
+++ src/main/java/frc/rebuilt/ShotCalculator.java
@@ -1,13 +1,15 @@
 package frc.rebuilt;
 
+import dev.doglog.DogLog;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.Robot;
 
 public class ShotCalculator {
@@ -17,33 +19,52 @@ public class ShotCalculator {
   private static final Transform2d robotToTurret = new Transform2d();
 
   public static ShotCalculator getInstance() {
-    if (instance == null) instance = new ShotCalculator();
+    if (instance == null)
+      instance = new ShotCalculator();
     return instance;
   }
 
   public record ShootingParameters(
       Rotation2d turretAngle,
       double hoodAngle,
-      double flywheelSpeed) {}
+      double flywheelSpeed) {
+  }
 
   private ShootingParameters latestParameters = null;
 
-  private static final InterpolatingTreeMap<Double, Rotation2d> shotHoodAngleMap =
-      new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Rotation2d::interpolate);
+  private static double phaseDelay;
 
-  private static final InterpolatingDoubleTreeMap shotFlywheelSpeedMap =
-      new InterpolatingDoubleTreeMap();
+  private static final InterpolatingTreeMap<Double, Rotation2d> shotHoodAngleMap = new InterpolatingTreeMap<>(
+      InverseInterpolator.forDouble(), Rotation2d::interpolate);
+
+  private static final InterpolatingDoubleTreeMap shotFlywheelSpeedMap = new InterpolatingDoubleTreeMap();
+
+  private static final InterpolatingDoubleTreeMap timeOfFlightMap = new InterpolatingDoubleTreeMap();
 
   static {
+    phaseDelay = 0.03;
+
     shotHoodAngleMap.put(1.8122, Rotation2d.fromDegrees(50.0));
     shotHoodAngleMap.put(2.612079, Rotation2d.fromDegrees(55.0));
     shotHoodAngleMap.put(3.75661, Rotation2d.fromDegrees(60.0));
     shotHoodAngleMap.put(4.96786, Rotation2d.fromDegrees(65.0));
 
-    shotFlywheelSpeedMap.put(1.8122, 200.0);
-    shotFlywheelSpeedMap.put(2.612079, 210.0);
-    shotFlywheelSpeedMap.put(3.75661, 230.0);
-    shotFlywheelSpeedMap.put(4.96786, 260.0);
+    shotFlywheelSpeedMap.put(1.34, 210.0);
+    shotFlywheelSpeedMap.put(1.78, 220.0);
+    shotFlywheelSpeedMap.put(2.17, 220.0);
+    shotFlywheelSpeedMap.put(2.81, 230.0);
+    shotFlywheelSpeedMap.put(3.82, 250.0);
+    shotFlywheelSpeedMap.put(4.09, 255.0);
+    shotFlywheelSpeedMap.put(4.40, 260.0);
+    shotFlywheelSpeedMap.put(4.77, 265.0);
+    shotFlywheelSpeedMap.put(5.57, 275.0);
+    shotFlywheelSpeedMap.put(5.60, 290.0);
+
+    timeOfFlightMap.put(5.68, 1.16);
+    timeOfFlightMap.put(4.55, 1.12);
+    timeOfFlightMap.put(3.15, 1.11);
+    timeOfFlightMap.put(1.88, 1.09);
+    timeOfFlightMap.put(1.38, 0.90);
   }
 
   public ShootingParameters getParameters() {
@@ -54,37 +75,60 @@ public class ShotCalculator {
     // Target location on the field
     Translation2d target = HubTargetFactory.generate().toTranslation2d();
 
-    // Turret position on the field
-    Pose2d turretPose =
-        Robot.getSwerve().getRobotPose().transformBy(robotToTurret);
+    // Calculate estimated pose while accounting for phase delay
+    Pose2d robotPose = Robot.getSwerve().getRobotPose();
+    ChassisSpeeds robotRelativeVelocity = Robot.getSwerve().getCurrentRobotChassisSpeeds();
+    robotPose = robotPose.exp(
+        new Twist2d(
+            robotRelativeVelocity.vxMetersPerSecond * phaseDelay,
+            robotRelativeVelocity.vyMetersPerSecond * phaseDelay,
+            robotRelativeVelocity.omegaRadiansPerSecond * phaseDelay));
 
-    // Distance to target
-    double distance =
-        target.getDistance(turretPose.getTranslation());
+    // Calculate distance from turret to target
+    Pose2d turretPosition = robotPose.transformBy(robotToTurret);
+    double turretToTargetDistance = target.getDistance(turretPosition.getTranslation());
 
-    // Field-relative angle from turret to target
-    Rotation2d turretAngle =
-        target.minus(turretPose.getTranslation()).getAngle();
+    // Calculate field relative turret velocity
+    ChassisSpeeds robotVelocity = ChassisSpeeds.fromRobotRelativeSpeeds(robotRelativeVelocity,
+        Robot.getSwerve().getRobotPose().getRotation());
+    double robotAngle = robotPose.getRotation().getRadians();
+    double turretVelocityX = robotVelocity.vxMetersPerSecond
+        + robotVelocity.omegaRadiansPerSecond
+            * (robotToTurret.getY() * Math.cos(robotAngle)
+                - robotToTurret.getX() * Math.sin(robotAngle));
+    double turretVelocityY = robotVelocity.vyMetersPerSecond
+        + robotVelocity.omegaRadiansPerSecond
+            * (robotToTurret.getX() * Math.cos(robotAngle)
+                - robotToTurret.getY() * Math.sin(robotAngle));
 
-    double hoodAngle =
-        shotHoodAngleMap.get(distance).getRadians();
+    // Account for imparted velocity by robot (turret) to offset
+    double timeOfFlight;
+    Pose2d lookaheadPose = turretPosition;
+    double lookaheadTurretToTargetDistance = turretToTargetDistance;
+    for (int i = 0; i < 20; i++) {
+      timeOfFlight = timeOfFlightMap.get(lookaheadTurretToTargetDistance);
+      double offsetX = turretVelocityX * timeOfFlight;
+      double offsetY = turretVelocityY * timeOfFlight;
+      lookaheadPose = new Pose2d(
+          turretPosition.getTranslation().plus(new Translation2d(offsetX, offsetY)),
+          turretPosition.getRotation());
+      lookaheadTurretToTargetDistance = target.getDistance(lookaheadPose.getTranslation());
+    }
+    // Calculate parameters accounted for imparted velocity
+    Rotation2d turretAngle = target.minus(lookaheadPose.getTranslation()).getAngle();
+    double hoodAngle = shotHoodAngleMap.get(lookaheadTurretToTargetDistance).getRadians();
+    double flywheelSpeed = shotFlywheelSpeedMap.get(lookaheadTurretToTargetDistance);
 
-    double flywheelSpeed =
-        shotFlywheelSpeedMap.get(distance);
+    latestParameters = new ShootingParameters(
+        turretAngle,
+        hoodAngle,
+        flywheelSpeed);
 
-    latestParameters =
-        new ShootingParameters(
-            turretAngle,
-            hoodAngle,
-            flywheelSpeed);
-    
-    
-    SmartDashboard.putString("ShotCalc/Target", target.toString());
-    SmartDashboard.putNumber("ShotCalc/DistanceMeters", distance);
-    SmartDashboard.putNumber("ShotCalc/TurretAngleDeg", turretAngle.getDegrees());
-    SmartDashboard.putNumber("ShotCalc/HoodAngleRad", hoodAngle);
-    SmartDashboard.putNumber("ShotCalc/HoodAngleDeg", Math.toDegrees(hoodAngle));
-    SmartDashboard.putNumber("ShotCalc/FlywheelSpeed", flywheelSpeed);
+    DogLog.log("ShotCalc/DistanceMeters", Double.toString(lookaheadTurretToTargetDistance));
+    DogLog.log("ShotCalc/LookaheadPose", lookaheadPose);
+    DogLog.log("ShotCalc/TurretAngleDeg", Double.toString(turretAngle.getDegrees()));
+    DogLog.log("ShotCalc/HoodAngleDeg", Double.toString(Math.toDegrees(hoodAngle)));
+    DogLog.log("ShotCalc/FlywheelSpeed", Double.toString(flywheelSpeed));
     return latestParameters;
   }
 
