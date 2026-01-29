e0e80379494f8722349eb64f670880252274c49a
Praiseafrog <elliottckyle@outlook.com>
2026-01-29 17:34:20 -0600

    workin hard or hardly workin

--- src/main/java/frc/spectrumLib/leds/SpectrumLEDs2.java
+++ src/main/java/frc/spectrumLib/leds/SpectrumLEDs2.java
@@ -5,20 +5,47 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.LEDConfigs;
 import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.StrobeAnimation;
 import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.RGBWColor;
 import com.ctre.phoenix6.signals.StripTypeValue;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import lombok.Getter;
+import lombok.Setter;
 
 public class SpectrumLEDs2 extends SubsystemBase{
     CANdle leds;
     LEDConfigs configs;
 
-    public static final int ledsID = 61;
-    
+    public static class Config {
+        @Getter private String name;
+        @Getter @Setter private boolean attached = true;
+        @Getter @Setter private int length;
+
+        public Config(String name, int length) {
+            this.name = name;
+            this.length = length;
+        }
+    }
+
+    @Getter private Config config;
     public static final int LedEndIndex = 120;
+
+    protected final ControlRequest defaultPattern = new StrobeAnimation(0, LedEndIndex).withColor(new RGBWColor(Color.kGreen)).withFrameRate(500);
+
+    @Getter
+    protected Command defaultCommand =
+            setPattern(defaultPattern, -1).withName("LEDs.defaultCommand");
+    
+    public final Trigger defaultTrigger = new Trigger(() -> defaultCommand.isScheduled());
+
+    @Getter @Setter private int commandPriority = 0;
+
+    public static final int ledsID = 61; //TODO: idk what this actually does so change that number to be significant
     
     public static void tryUntilOk(int maxAttempts, Supplier<StatusCode> command) {
     for (int i = 0; i < maxAttempts; i++) {
@@ -30,15 +57,19 @@ public class SpectrumLEDs2 extends SubsystemBase{
     public SpectrumLEDs2() {
         leds = new CANdle(ledsID);
         configs = new LEDConfigs();
-        configs.StripType = StripTypeValue.GRB;
+        configs.StripType = StripTypeValue.RGB;
         tryUntilOk(5, () -> leds.getConfigurator().apply(configs));
     }
 
-    public void setLEDS(ControlRequest request) {
+    public void setLEDs(ControlRequest request) {
             leds.setControl(request);
     }
     
-    public Command setLedCommand(ControlRequest request){
-        return Commands.runOnce(()-> setLEDS(request));
+    public Command setPattern(ControlRequest pattern, int priority) {
+        return run(() -> {
+                    commandPriority = priority;
+                    setLEDs(pattern);})
+                .ignoringDisable(true)
+                .withName("LEDs.setPattern");
     }
 }
