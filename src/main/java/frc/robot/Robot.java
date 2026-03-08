54e817087dc9415673b6c916d41fc0a6003ea75d
Spectrum <Spectrum3847@gmail.com>
2026-03-07 21:33:18 -0600

    Revert "start of led code"
    This reverts commit 67d0a39705c60e3e19e98773f6c2a2aa07b4cf8f.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,7 +37,8 @@ import frc.robot.intakeExtension.IntakeExtension;
 import frc.robot.intakeExtension.IntakeExtension.IntakeExtensionConfig;
 import frc.robot.launcher.Launcher;
 import frc.robot.launcher.Launcher.LauncherConfig;
-import frc.robot.leds.LEDSubsystem;
+import frc.robot.leds.LedFull;
+import frc.robot.leds.LedFull.LedFullConfig;
 import frc.robot.operator.Operator;
 import frc.robot.operator.Operator.OperatorConfig;
 import frc.robot.pilot.Pilot;
@@ -83,6 +84,7 @@ public class Robot extends SpectrumRobot {
         public PilotConfig pilot = new PilotConfig();
         public OperatorConfig operator = new OperatorConfig();
         public FuelIntakeConfig fuelIntake = new FuelIntakeConfig();
+        public LedFullConfig leds = new LedFullConfig();
         public RotationalPivotConfig turret = new RotationalPivotConfig();
         public IntakeExtensionConfig intakeExtension = new IntakeExtensionConfig();
         public IndexerTowerConfig indexerTower = new IndexerTowerConfig();
@@ -97,7 +99,7 @@ public class Robot extends SpectrumRobot {
     @Getter private static IntakeExtension intakeExtension;
     @Getter private static IndexerTower indexerTower;
     @Getter private static IndexerBed indexerBed;
-    @Getter private static LEDSubsystem leds;
+    @Getter private static LedFull leds;
     @Getter private static Operator operator;
     @Getter private static Pilot pilot;
     @Getter private static VisionSystem visionSystem;
@@ -137,7 +139,7 @@ public class Robot extends SpectrumRobot {
              */
             double canInitDelay = 0.1; // Delay between any mechanism with motor/can configs
 
-            leds = new LEDSubsystem();
+            leds = new LedFull(config.leds);
             operator = new Operator(config.operator);
             pilot = new Pilot(config.pilot);
             swerve = new Swerve(config.swerve);
--- src/main/java/frc/robot/leds/LedStates.java
+++ src/main/java/frc/robot/leds/LedStates.java
@@ -3,23 +3,33 @@ package frc.robot.leds;
 import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.signals.*;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.rebuilt.ShiftHelpers;
+import frc.robot.Robot;
+import frc.spectrumLib.leds.SpectrumLEDs;
 import frc.spectrumLib.util.Util;
 
 public class LedStates extends LedCANdle {
+    private static LedFull leds = Robot.getLeds();
+    private static LedRight right = leds.getRight();
+    private static LedLeft left = leds.getLeft();
 
     static double currentMatchTime = DriverStation.getMatchTime();
 
     static void bindTriggers() {
-
-        disabledPattern(Util.disabled.and(Util.dsAttached), 10);
+        // disabledPattern(Util.disabled.and(Util.dsAttached));
+        teleopPattern(Util.teleop.and(Util.dsAttached));
+        autoPattern(Util.autoMode.and(Util.dsAttached));
+        testModePattern(Util.testMode.and(Util.dsAttached));
 
         // Match time related patterns
         redAlliance(Util.teleop.and(() -> !ShiftHelpers.isCurrentShiftBlue(currentMatchTime)), 10);
         blueAlliance(Util.teleop.and(() -> ShiftHelpers.isCurrentShiftBlue(currentMatchTime)), 10);
-        shiftSwitchBlink(
+        blinkyBlonk(
                 Util.teleop.and(
                         () -> {
                             double t = DriverStation.getMatchTime();
@@ -45,7 +55,9 @@ public class LedStates extends LedCANdle {
                                 ShiftHelpers.blueWonAuto()
                                         ? new RGBWColor(0, 0, 255)
                                         : new RGBWColor(255, 0, 0));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(shiftAnimationRight)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(shiftAnimationRight)));
         SingleFadeAnimation shiftAnimationLeft =
                 new SingleFadeAnimation(kSlot1StartIdx, kSlot1EndIdx)
                         .withSlot(1)
@@ -53,7 +65,9 @@ public class LedStates extends LedCANdle {
                                 ShiftHelpers.blueWonAuto()
                                         ? new RGBWColor(0, 0, 255)
                                         : new RGBWColor(255, 0, 0));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(shiftAnimationLeft)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(shiftAnimationLeft)));
     }
 
     static void redAlliance(Trigger trigger, int priority) {
@@ -61,12 +75,16 @@ public class LedStates extends LedCANdle {
                 new SingleFadeAnimation(kSlot0StartIdx, kSlot0EndIdx)
                         .withSlot(0)
                         .withColor(new RGBWColor(255, 0, 0));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(redAllianceShiftRight)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(redAllianceShiftRight)));
         SingleFadeAnimation redAllianceShiftLeft =
                 new SingleFadeAnimation(kSlot1StartIdx, kSlot1EndIdx)
                         .withSlot(1)
                         .withColor(new RGBWColor(255, 0, 0));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(redAllianceShiftLeft)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(redAllianceShiftLeft)));
     }
 
     static void blueAlliance(Trigger trigger, int priority) {
@@ -74,16 +92,20 @@ public class LedStates extends LedCANdle {
                 new SingleFadeAnimation(kSlot0StartIdx, kSlot0EndIdx)
                         .withSlot(0)
                         .withColor(new RGBWColor(0, 0, 255));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(blueAllianceShiftRight)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(blueAllianceShiftRight)));
         SingleFadeAnimation blueAllianceShiftLeft =
                 new SingleFadeAnimation(kSlot1StartIdx, kSlot1EndIdx)
                         .withSlot(1)
                         .withColor(new RGBWColor(0, 0, 255));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(blueAllianceShiftLeft)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(blueAllianceShiftLeft)));
     }
 
     // shows upcoming alliance shift 3 seconds before and 3 seconds after
-    static void shiftSwitchBlink(Trigger trigger, int priority) {
+    static void blinkyBlonk(Trigger trigger, int priority) {
         StrobeAnimation aboutToShiftRight =
                 new StrobeAnimation(kSlot0StartIdx, kSlot0EndIdx)
                         .withSlot(0)
@@ -91,7 +113,9 @@ public class LedStates extends LedCANdle {
                                 !ShiftHelpers.isCurrentShiftBlue(currentMatchTime)
                                         ? new RGBWColor(0, 0, 255)
                                         : new RGBWColor(255, 0, 0));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(aboutToShiftRight)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(aboutToShiftRight)));
         StrobeAnimation aboutToShiftLeft =
                 new StrobeAnimation(kSlot1StartIdx, kSlot1EndIdx)
                         .withSlot(1)
@@ -99,7 +123,9 @@ public class LedStates extends LedCANdle {
                                 !ShiftHelpers.isCurrentShiftBlue(currentMatchTime)
                                         ? new RGBWColor(0, 0, 255)
                                         : new RGBWColor(255, 0, 0));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(aboutToShiftLeft)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(aboutToShiftLeft)));
     }
 
     static void endgame(Trigger trigger, int priority) {
@@ -107,17 +133,57 @@ public class LedStates extends LedCANdle {
                 new SingleFadeAnimation(kSlot0StartIdx, kSlot0EndIdx)
                         .withSlot(0)
                         .withColor(new RGBWColor(207, 255, 4));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(endgameAnimationRight)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(endgameAnimationRight)));
         SingleFadeAnimation endgameAnimationLeft =
                 new SingleFadeAnimation(kSlot1StartIdx, kSlot1EndIdx)
                         .withSlot(1)
                         .withColor(new RGBWColor(207, 255, 4));
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(endgameAnimationLeft)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(endgameAnimationLeft)));
     }
 
-    static void disabledPattern(Trigger trigger, int priority) {
+    /** Default LED commands for each mode */
+    private static Trigger ledDefaultCommand(
+            String name, SpectrumLEDs sLeds, LEDPattern pattern, Trigger trigger) {
+        int priority = -1;
+        return trigger.and(sLeds.checkPriority(priority), sLeds.defaultTrigger)
+                // .onTrue(sLeds.setPattern(pattern, priority).withName(name));
+                .onTrue(sLeds.setPattern(pattern, priority));
+    }
+
+    static void disabledPattern(Trigger trigger) {
+        ledDefaultCommand(
+                "right.disabledPattern", right, right.ombre(right.purple, right.white), trigger);
+        ledDefaultCommand(
+                "left.disabledPattern", left, left.ombre(left.purple, left.white), trigger);
+    }
+
+    static void betterDisabledPattern(Trigger trigger, int priority) {
         FireAnimation disabledFire = new FireAnimation(kSlot0StartIdx, kSlot0EndIdx).withSlot(0);
-        trigger.onTrue(new InstantCommand(() -> candle.setControl(disabledFire)));
+        trigger.onTrue(
+                new InstantCommand(
+                        () -> candle.setControl(disabledFire)));
+    }
+
+    static void teleopPattern(Trigger trigger) {
+        ledDefaultCommand("right.teleopPattern", right, right.bounce(right.purple, 3), trigger);
+        ledDefaultCommand("left.teleopPattern", left, left.bounce(left.purple, 3), trigger);
+    }
+
+    static void autoPattern(Trigger trigger) {
+        ledDefaultCommand(
+                "right.autoPattern", right, right.countdown(Timer::getFPGATimestamp, 15), trigger);
+
+        ledDefaultCommand(
+                "left.autoPattern", left, left.countdown(Timer::getFPGATimestamp, 15), trigger);
+    }
+
+    static void testModePattern(Trigger trigger) {
+        ledDefaultCommand("right.testModePattern", right, right.chase(Color.kRed, 0.2, 1), trigger);
+        ledDefaultCommand("left.testModePattern", left, left.chase(Color.kRed, 0.2, 1), trigger);
     }
 
     /** LED non-default Commands, set the priority value to see which command takes precedence */
