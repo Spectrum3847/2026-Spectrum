51e5326799c9a11af7b1609806f2e684a27f3c77
project516 <138796702+Project516@users.noreply.github.com>
2026-03-16 17:18:14 -0500

    [photon] add TODO for setting swerve and turret configs

--- src/main/java/frc/robot/configs/PHOTON2026.java
+++ src/main/java/frc/robot/configs/PHOTON2026.java
@@ -6,9 +6,10 @@ public class PHOTON2026 extends Config {
 
     // Photon Machine
     public PHOTON2026() {
-
         super();
+        // TODO: set values
         // swerve.configEnconderOffsets();
+        // turret.setCANcoderOffset();
 
         // Attached Mechanisms
         pilot.setAttached(true);
