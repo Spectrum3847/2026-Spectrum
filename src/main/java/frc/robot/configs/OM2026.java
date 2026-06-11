b7ca865441457b2be5adf06aece6901f85dd6a92
JediMasterLucy <lucia.hans@icloud.com>
2026-06-11 21:04:21 +0000

    Add OM2026 configuration class for robot setup

---
+++ src/main/java/frc/robot/configs/OM2026.java
@@ -0,0 +1,16 @@
+package frc.robot.configs;
+
+import frc.robot.Robot.Config;
+
+public class OM2026 extends Config {
+    public OM2026() {
+        super();
+
+        // TODO: change when robot is built and can be updated
+        swerve.configEncoderOffsets(
+            0, 0, 0, 0);
+        
+        pilot.setAttached(true);
+        operator.setAttached(true);
+    }
+}
