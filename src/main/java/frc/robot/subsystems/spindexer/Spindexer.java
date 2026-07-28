e12a8d28ceba6e583e40eaa238a19d5f770a2b33
JediMasterLucy <lucia.hans@icloud.com>
2026-07-28 11:13:02 -0500

    - stole the IntakeExtension from FM - worked on Spindexer subsystem, its almost done
    next -> intake and intake extension if CAD is complete

--- src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/spindexer/Spindexer.java
@@ -1,10 +1,15 @@
 package frc.robot.subsystems.spindexer;
 
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import frc.robot.RobotSim;
+import frc.robot.subsystems.intakeExtension.IntakeExtension.IntakeExtensionConfig;
+import frc.robot.subsystems.intakeExtension.IntakeExtension.IntakeExtensionRight.RightConfig;
+import frc.robot.subsystems.spindexer.Spindexer.SpindexerFront.FrontConfig;
 import frc.spectrumLib.hardware.Rio;
 import frc.spectrumLib.mechanism.Mechanism;
 import frc.spectrumLib.sim.RollerConfig;
@@ -19,6 +24,9 @@ public class Spindexer extends Mechanism {
 
         @Getter @Setter private double supplyCurrentLimit = 40;
         @Getter @Setter private double statorCurrentLimit = 80;
+
+        @Getter @Setter private double gearRatio = 37.5;
+
         // TODO: tune
         @Getter @Setter private double velocityKp = 5;
         @Getter @Setter private double velocityKv = 10;
@@ -34,19 +42,67 @@ public class Spindexer extends Mechanism {
         @Getter @Setter private double spindexerDiameter = 12;
 
         public SpindexerConfig() {
-            super("Spindexer 1", 8, Rio.CANIVORE);
+            super("Spindexer Back", 8, Rio.CANIVORE);
             configPIDGains(velocityKp, 0, 0);
             configFeedForwardGains(velocityKs, velocityKv, 0, 0);
-            configGearRatio(1);
+            configGearRatio(gearRatio);
             configSupplyCurrentLimit(supplyCurrentLimit, true);
             configStatorCurrentLimit(statorCurrentLimit, true);
             configForwardTorqueCurrentLimit(statorCurrentLimit);
             configReverseTorqueCurrentLimit(statorCurrentLimit);
             configNeutralBrakeMode(false);
             configCounterClockwise_Positive();
-            setFollowerConfigs(
-                    new FollowerConfig(
-                            "Spindexer 2", 9, Rio.CANIVORE, MotorAlignmentValue.Aligned));
+        }
+    }
+
+    public static class SpindexerFront extends Mechanism {
+        public static class FrontConfig extends Config {
+
+            @Getter @Setter private double supplyCurrentLimit = 40;
+            @Getter @Setter private double statorCurrentLimit = 80;
+
+            @Getter @Setter private double gearRatio = 1.833;
+
+            // TODO: tune
+            @Getter @Setter private double velocityKp = 5;
+            @Getter @Setter private double velocityKv = 10;
+            @Getter @Setter private double velocityKs = 15;
+
+            public FrontConfig() {
+                super("Spindexer Front", 9, Rio.CANIVORE);
+                configPIDGains(velocityKp, 0, 0);
+                configFeedForwardGains(velocityKs, velocityKv, 0, 0);
+                configGearRatio(gearRatio);
+                configSupplyCurrentLimit(supplyCurrentLimit, true);
+                configStatorCurrentLimit(statorCurrentLimit, true);
+                configForwardTorqueCurrentLimit(statorCurrentLimit);
+                configReverseTorqueCurrentLimit(statorCurrentLimit);
+                configNeutralBrakeMode(false);
+                configCounterClockwise_Positive();
+            }
+        }
+
+        @Getter private final FrontConfig frontConfig;
+
+        public SpindexerFront(SpindexerConfig backConfig) {
+            super(new FrontConfig(backConfig));
+            this.frontConfig = (FrontConfig) super.config;
+            Telemetry.print(getName() + " Subsystem Initialized");
+        }
+
+        public void spin(double rpm) {
+            setVelocityTCFOCrpm(() -> rpm);
+        }
+
+        @Override
+        public void periodic() {
+            logBatteryUsage();
+            Telemetry.log("Spindexer/CurrentCommand", getCurrentCommandName());
+            Telemetry.log("Spindexer/Voltage", getVoltage(), "volts");
+            Telemetry.log("Spindexer/StatorCurrent", getStatorCurrent(), "amps");
+            Telemetry.log("Spindexer/SupplyCurrent", getSupplyCurrent(), "amps");
+            Telemetry.log("Spindexer/RPM", getVelocityRPM(), "RPM");
+            Telemetry.log("Spindexer/Temp", getTemp(), "deg_C");
         }
     }
 
@@ -84,31 +140,44 @@ public class Spindexer extends Mechanism {
 
     // TODO: get actual values when robot is built
     private void applyStates() {
-        double wantedRPM = 0;
+        double wantedRPMSpin = 0;
+        double wantedRPMIndex = 0;
         switch (systemState) {
             case OFF:
                 stop();
                 return;
             case INDEX_MAX:
-                wantedRPM = 3000;
+                wantedRPMSpin = 3000;
+                wantedRPMIndex = 3000;
                 break;
             case IDLE_SLOW_INDEX:
-                wantedRPM = 1000;
+                wantedRPMSpin = 1000;
+                wantedRPMIndex = 1000;
                 break;
             case UNJAM:
-                wantedRPM = -2000;
+                wantedRPMSpin = -2000;
+                wantedRPMIndex = -2000;
                 break;
         }
-        final double finalWantedRPM = wantedRPM;
-        setVelocityTCFOCrpm(() -> finalWantedRPM);
+        final double finalWantedRPMSpin = wantedRPMSpin;
+        final double finalWantedRPMIndex = wantedRPMIndex;
+        commandBoth(finalWantedRPMSpin, finalWantedRPMIndex);
     }
 
     @Getter private final SpindexerConfig config;
     @Getter private SpindexerSim sim;
+    private final SpindexerFront front;
+
+    // TODO: test
+    private void commandBoth(double spinRPM, double indexRPM) {
+        setVelocityTCFOCrpm(() -> spinRPM);
+        front.spin(indexRPM);
+    }
 
     public Spindexer(SpindexerConfig config) {
         super(config);
         this.config = config;
+        this.front = new SpindexerFront(config);
 
         simulationInit();
         Telemetry.print(getName() + " Subsystem Initialized");
