e90c42e44067493cc30031613496e52600ff5c94
Spectrum <Spectrum3847@gmail.com>
2026-08-19 17:38:42 -0500

    Adjusted limits for new turret config.

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -58,8 +58,8 @@ public class Turret extends Mechanism {
         @Getter private final double mmJerk = 0;
         @Getter private final double peakVoltage = 6;
 
-        @Getter private final double reverseLimitDegrees = -35;
-        @Getter private final double forwardLimitDegrees = 350;
+        @Getter private final double reverseLimitDegrees = -0.5;
+        @Getter private final double forwardLimitDegrees = .45;
 
         @Getter private final double sensorToMechanismRatio = 39.78;
 
