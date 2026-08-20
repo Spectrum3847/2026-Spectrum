8a78d5c60b9ee3d592772587ccd5c25b0e1a8f3c
JediMasterLucy <lucia.hans@icloud.com>
2026-08-19 19:53:21 -0500

    started on feed left vs right buttons, made Commands and buttons. next: test in sim

--- src/main/java/frc/rebuilt/targetFactories/FeedTargetFactory.java
+++ src/main/java/frc/rebuilt/targetFactories/FeedTargetFactory.java
@@ -5,6 +5,8 @@ import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.math.interpolation.Interpolator;
 import edu.wpi.first.math.interpolation.InverseInterpolator;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.rebuilt.Field;
 import frc.robot.Robot;
 import frc.robot.subsystems.swerve.Swerve;
@@ -12,6 +14,7 @@ import frc.robot.subsystems.swerve.Swerve;
 public class FeedTargetFactory {
 
     private static final Swerve swerve = Robot.getSwerve();
+    private static boolean inFieldLeft = true;
 
     static InterpolatingTreeMap<Double, Double> distanceOffsetMap =
             new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Interpolator.forDouble());
@@ -23,7 +26,7 @@ public class FeedTargetFactory {
     static Double kXDistanceOffset = Units.inchesToMeters(0);
     /** Generate. */
     public static Translation2d generate() {
-        boolean inFieldLeft = swerve.inFieldLeft().getAsBoolean();
+        inFieldLeft = swerve.inFieldLeft().getAsBoolean();
         boolean inOpposingAllianceZone = swerve.inEnemyAllianceZone().getAsBoolean();
         Translation2d feedTarget;
 
@@ -57,5 +60,13 @@ public class FeedTargetFactory {
                 new Translation2d(
                         feedTarget.getX() + offSet.getX(), feedTarget.getY() + offSet.getY());
         return feedTarget;
+    }
+
+    public static Command feedLeft() {
+        return Commands.runOnce(() -> inFieldLeft = true);
+    }
+
+    public static Command feedRight() {
+        return Commands.runOnce(() -> inFieldLeft = false);
     }
 }
